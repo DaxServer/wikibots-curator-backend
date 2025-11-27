faba527bd6206b17008db8efaaafa6355ab427db
DaxServer <daxserver@icloud.com>
2025-11-27 21:34:12 +0100

    fix batches migration

--- alembic/versions/47e3273eb7e7_batches_primary_key_migration.py
+++ alembic/versions/47e3273eb7e7_batches_primary_key_migration.py
@@ -22,24 +22,12 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     with op.batch_alter_table("batches") as batch_op:
-        # Make batch_uid non-nullable (it was PK)
-        batch_op.alter_column(
-            "batch_uid", existing_type=sa.String(length=255), nullable=False
-        )
-
         # Make id non-nullable and auto-increment
         batch_op.alter_column(
             "id", existing_type=sa.Integer(), nullable=False, autoincrement=True
         )
 
-        # Create new Primary Key on 'id'
-        batch_op.create_primary_key("pk_batches", ["id"])
-
-        # Create Unique Index on 'batch_uid'
-        batch_op.create_index("ix_batches_batch_uid", ["batch_uid"], unique=True)
-
     # 6. Modify 'upload_requests' table: Add new FK
-
     with op.batch_alter_table("upload_requests") as batch_op:
         # Add new FK
         batch_op.create_foreign_key(
